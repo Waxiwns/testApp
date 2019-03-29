34aa1656f77c98e9e0f7d775c6c43efa8c0564d8
waxiwns <mxmci525@gmail.com>
2019-03-29 09:58:31 +0200

    CircleCI yml

--- src/test/java/web/LoginTests.java
+++ src/test/java/web/LoginTests.java
@@ -27,112 +27,106 @@ public class LoginTests {
 
     @Before
     public void setConfiguration(){
-        System.out.println("Before start");
-
         Configuration.headless = headless;
-        System.out.println("Before end");
     }
 
     @Test
     public void loginWithValidValues() {
-        System.out.println("Start test");
         step.openLoginPage();
-        System.out.println("open Login Page");
         step.logInWithCorrectValues(id, pass);
-        System.out.println("log In With Correct Values");
     }
 
-//    @Test
-//    public void loginWithUppercaseId() {
-//        step.openLoginPage();
-//        step.logInWithCorrectValues(id.toUpperCase(), pass);
-//    }
-//
-//    @Test
-//    public void loginWithSpacesBeforeAndAfterId() {
-//        id = " " + id + " ";
-//
-//        step.openLoginPage();
-//        step.logInWithCorrectValues(id, pass);
-//    }
-//
-//    @Test
-//    public void loginWithIncorrectPass() {
-//        step.openLoginPage();
-//        step.logInWithIncorrectValues(id, incorrectValue);
-//        step.errorMsgDisappeared();
-//    }
-//
-//    @Test
-//    public void loginWithIncorrectId() {
-//        step.openLoginPage();
-//        step.logInWithIncorrectValues(incorrectValue, pass);
-//    }
-//
-//    @Test
-//    public void loginWithIncorrectValues() {
-//        step.openLoginPage();
-//        step.logInWithIncorrectValues(incorrectValue, incorrectValue);
-//    }
-//
-//    @Test
-//    public void loginWithConverselyValues() {
-//        step.openLoginPage();
-//        step.logInWithIncorrectValues(pass, id);
-//    }
-//
-//    @Test
-//    public void changeLocale() {
-//        step.openLoginPage();
-//        step.shouldBeLocale(localeEn);
-//        step.changeLocaleTo(localeAr);
-//        step.shouldBeLocale(localeAr);
-//        step.changeLocaleTo(localeEn);
-//        step.shouldBeLocale(localeEn);
-//    }
-//
-//    @Test
-//    public void changeLocaleErrorMsg() {
-//        step.openLoginPage();
-//        step.shouldBeLocale(localeEn);
-//
-////        login with incorrect values to show error message
-//        step.logInWithIncorrectValues(incorrectValue, incorrectValue);
-//        step.errorMsgShouldBeLocale(localeEn);
-//
-//        step.changeLocaleTo(localeAr);
-//        step.errorMsgShouldBeLocale(localeAr);
-//    }
-//
-//    @Test
-//    public void correctDashboardLocale() {
-//        step.openLoginPage();
-//        step.shouldBeLocale(localeEn);
-//
-////        login with correct values
-//        step.logIn(id, pass);
-//
-////        expect locale
-//        step.dashboardMsgShouldBeLocale(localeEn);
-//
-//        step.closeMsgForm();
-//        step.logOut();
-//
-////        change locale to arabic
-//        step.changeLocaleTo(localeAr);
-//        step.shouldBeLocale(localeAr);
-//
-////        login with correct values
-//        step.logIn(id, pass);
-//
-////        expect locale
-//        step.dashboardMsgShouldBeLocale(localeAr);
-//    }
-//    @Test
-//    public void jsExecute(){
-//        step.openLoginPage();
-//        step.visitPage("http://qaru.site/questions/16404796/what-is-javascriptexecutor-in-selenium");
-//    }
+    @Test
+    public void loginWithUppercaseId() {
+        step.openLoginPage();
+        step.logInWithCorrectValues(id.toUpperCase(), pass);
+    }
+
+    @Test
+    public void loginWithSpacesBeforeAndAfterId() {
+        id = " " + id + " ";
+
+        step.openLoginPage();
+        step.logInWithCorrectValues(id, pass);
+    }
+
+    @Test
+    public void loginWithIncorrectPass() {
+        step.openLoginPage();
+        step.logInWithIncorrectValues(id, incorrectValue);
+        step.errorMsgDisappeared();
+    }
+
+    @Test
+    public void loginWithIncorrectId() {
+        step.openLoginPage();
+        step.logInWithIncorrectValues(incorrectValue, pass);
+    }
+
+    @Test
+    public void loginWithIncorrectValues() {
+        step.openLoginPage();
+        step.logInWithIncorrectValues(incorrectValue, incorrectValue);
+    }
+
+    @Test
+    public void loginWithConverselyValues() {
+        step.openLoginPage();
+        step.logInWithIncorrectValues(pass, id);
+    }
+
+    @Test
+    public void changeLocale() {
+        step.openLoginPage();
+        step.shouldBeLocale(localeEn);
+        step.changeLocaleTo(localeAr);
+        step.shouldBeLocale(localeAr);
+        step.changeLocaleTo(localeEn);
+        step.shouldBeLocale(localeEn);
+    }
+
+    @Test
+    public void changeLocaleErrorMsg() {
+        step.openLoginPage();
+        step.shouldBeLocale(localeEn);
+
+//        login with incorrect values to show error message
+        step.logInWithIncorrectValues(incorrectValue, incorrectValue);
+        step.errorMsgShouldBeLocale(localeEn);
+
+        step.changeLocaleTo(localeAr);
+        step.errorMsgShouldBeLocale(localeAr);
+    }
+
+    @Test
+    public void correctDashboardLocale() {
+        step.openLoginPage();
+        step.shouldBeLocale(localeEn);
+
+//        login with correct values
+        step.logIn(id, pass);
+
+//        expect locale
+        step.dashboardMsgShouldBeLocale(localeEn);
+
+        step.closeMsgForm();
+        step.logOut();
+
+//        change locale to arabic
+        step.changeLocaleTo(localeAr);
+        step.shouldBeLocale(localeAr);
+
+//        login with correct values
+        step.logIn(id, pass);
+
+//        expect locale
+        step.dashboardMsgShouldBeLocale(localeAr);
+    }
+    @Test
+    public void jsExecute(){
+        step.openLoginPage();
+        step.visitPage("http://qaru.site/questions/16404796/what-is-javascriptexecutor-in-selenium");
+    }
 
     @After
     public void closeBrowser(){
